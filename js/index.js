0a05e7d47e9289b02ff5e594842b44fcecf7ab4d
s930415 <s930415@yahoo.com.tw>
2021-06-11 21:26:43 +0800

    finish

--- js/index.js
+++ js/index.js
@@ -6,50 +6,65 @@ var app = new Vue({
     exam_index: 0,
     message: 123,
     exams: [{ img: "" }],
-    answers_count: 0
+    answers_count: 0,
+    correct_lock: false,
+    answers_lock: false,
   },
   methods: {
     chose(option) {
-      if (this.exams[this.exam_index].correct === option) {
-        right_ring.pause();
-        right_ring.currentTime = 0;
-        this.score += 1;
-        console.log("得分!!\n分數:" + this.score);
-        right_ring.play();
-        event.currentTarget.classList.add('correct');
-        document.getElementsByClassName('btn_01')[0].classList.remove('v-hide');
-        document.getElementsByClassName('btn_02')[0].classList.add('v-hide');
-      } else {
-        wrong_ring.pause();
-        wrong_ring.currentTime = 0;
-        wrong_ring.play();
-        event.currentTarget.classList.add('wrong');
-        let chosed_list = document.getElementsByClassName('chosed');
-        for(let i = 0 ; chosed_list.length > i ; i++){
-          chosed_list[i].classList.remove('v-hide');
+      if (!this.lock) {
+        if (this.exams[this.exam_index].correct === option) {
+          right_ring.pause();
+          right_ring.currentTime = 0;
+          if (!this.answers_lock) {
+            this.score += 1;
+            console.log("得分!!\n分數:" + this.score);
+          }
+          right_ring.play();
+          event.currentTarget.classList.add('correct');
+          document.getElementsByClassName('btn_01')[0].classList.remove('v-hide');
+          document.getElementsByClassName('btn_02')[0].classList.add('v-hide');
+          this.lock = true;
+        } else {
+          wrong_ring.pause();
+          wrong_ring.currentTime = 0;
+          wrong_ring.play();
+          event.currentTarget.classList.add('wrong');
+          let chosed_list = document.getElementsByClassName('chosed');
+          for (let i = 0; chosed_list.length > i; i++) {
+            chosed_list[i].classList.remove('v-hide');
+          }
         }
+        this.answers_count += 1;
       }
+      this.answers_lock = true;
     },
-    nextExam(){
-      document.getElementsByClassName('btn_01')[0].classList.add('v-hide');
-      document.getElementsByClassName('btn_02')[0].classList.add('v-hide');
-      card_list = document.getElementsByClassName('card');
-      for(let i = 0 ; card_list.length > i ; i++){
-        card_list[i].classList.remove('correct');
-        card_list[i].classList.remove('wrong');
-      }
-      if (this.exams.length - 1 > this.exam_index) {
-        this.exam_index = this.exam_index + 1;
+    nextExam() {
+      this.answers_lock = false;
+      this.lock = false;
+      if (this.answers_count >= 10) {
+        document.getElementsByClassName('btn_01')[0].classList.add('v-hide');
+        document.getElementsByClassName('btn_02')[0].classList.add('v-hide');
+        card_list = document.getElementsByClassName('card');
+        for (let i = 0; card_list.length > i; i++) {
+          card_list[i].classList.remove('correct');
+          card_list[i].classList.remove('wrong');
+        }
+        if (this.exams.length - 1 > this.exam_index) {
+          this.exam_index = this.exam_index + 1;
+        } else {
+          this.exam_index = 0;
+        }
+        shuffleArray(this.exams[this.exam_index].options);
       } else {
-        this.exam_index = 0;
+        this.goEndPage();
       }
-      shuffleArray(this.exams[this.exam_index].options);
     },
-    veiwAnswer(){
+    veiwAnswer() {
       document
         .getElementsByClassName("container_02")[0]
         .classList.remove("d-none");
-        document
+      document
         .getElementsByClassName("container_01")[0]
         .classList.add("d-none");
     },
@@ -59,6 +74,10 @@ var app = new Vue({
         .getElementsByClassName("game_area")[0]
         .classList.toggle("d-none");
       this.getData();
+    },
+    goEndPage() {
+      document.getElementsByClassName("intro")[0].classList.toggle("d-none");
+      document.getElementsByClassName("game_area")[0].classList.toggle("d-none");
     },
     getData() {
       let api = new XMLHttpRequest();
